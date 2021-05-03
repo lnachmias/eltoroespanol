bfb345b96651d65a2cfb2e637b2aaa0658ce14f7
Lior Nachmias <lior.nachmias@outlook.com>
2021-05-03 14:21:41 +0300

    AAAAAAAAAAAAAAAAA

--- api/bvll.py
+++ api/bvll.py
@@ -5,5 +5,5 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/html')
     self.end_headers()
-    self.wfile.write(<img-src="https://idsb.tmgrup.com.tr/ly/uploads/images/2020/10/20/66582.jpg"/).encode())
+    self.wfile.write(<img src="https://idsb.tmgrup.com.tr/ly/uploads/images/2020/10/20/66582.jpg">).encode())
     return
