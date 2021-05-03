48ea22c11db8739b1453d5350b4ea045a01f19d4
Lior Nachmias <lior.nachmias@outlook.com>
2021-05-03 14:14:00 +0300

    rip brain

--- api/bvll.py
+++ api/bvll.py
@@ -3,7 +3,7 @@ from datetime import datetime
 class handler(BaseHTTPRequestHandler):
   def do_GET(self):
     self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
+    self.send_header('Content-type', 'text/html')
     self.end_headers()
-    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+    self.wfile.write(<img-src="https://idsb.tmgrup.com.tr/ly/uploads/images/2020/10/20/66582.jpg"/).encode())
     return
